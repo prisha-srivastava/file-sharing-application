7ef7f4a8e97bfd07c46259e0f2f39214f0590557
Prisha Srivastava <63402702+prisha-srivastava@users.noreply.github.com>
2021-07-26 00:58:51 +0530

    committed

--- public/index.js
+++ public/index.js
@@ -11,13 +11,13 @@ const status = document.querySelector(".status");
 const sharingContainer = document.querySelector(".sharing-container");
 const copyURLBtn = document.querySelector("#copyURLBtn");
 const fileURL = document.querySelector("#fileURL");
-const emailForm = document.querySelector("#emailForm");
+// const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
 const baseURL = "https://file-sharing-ps25.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
-const emailURL = `${baseURL}/api/files/send`;
+// const emailURL = `${baseURL}/api/files/send`;
 
 const maxAllowedSize = 100 * 1024 * 1024; //100mb
 
@@ -121,8 +121,8 @@ const onFileUploadSuccess = (res) => {
   status.innerText = "Uploaded";
 
   // remove the disabled attribute from form btn & make text send
-  emailForm[2].removeAttribute("disabled");
-  emailForm[2].innerText = "Send";
+  // emailForm[2].removeAttribute("disabled");
+  // emailForm[2].innerText = "Send";
   progressContainer.style.display = "none"; // hide the box
 
   const { file: url } = JSON.parse(res);
@@ -131,36 +131,36 @@ const onFileUploadSuccess = (res) => {
   fileURL.value = url;
 };
 
-emailForm.addEventListener("submit", (e) => {
-  e.preventDefault(); // stop submission
+// emailForm.addEventListener("submit", (e) => {
+//   e.preventDefault(); // stop submission
 
-  // disable the button
-  emailForm[2].setAttribute("disabled", "true");
-  emailForm[2].innerText = "Sending";
+//   // disable the button
+//   emailForm[2].setAttribute("disabled", "true");
+//   emailForm[2].innerText = "Sending";
 
   const url = fileURL.value;
 
-  const formData = {
-    uuid: url.split("/").splice(-1, 1)[0],
-    emailTo: emailForm.elements["to-email"].value,
-    emailFrom: emailForm.elements["from-email"].value,
-  };
-  console.log(formData);
-  fetch(emailURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.success) {
-        showToast("Email Sent");
-        sharingContainer.style.display = "none"; // hide the box
-      }
-    });
-});
+//   const formData = {
+//     uuid: url.split("/").splice(-1, 1)[0],
+//     emailTo: emailForm.elements["to-email"].value,
+//     emailFrom: emailForm.elements["from-email"].value,
+//   };
+//   console.log(formData);
+//   fetch(emailURL, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.success) {
+//         showToast("Email Sent");
+//         sharingContainer.style.display = "none"; // hide the box
+//       }
+//     });
+// });
 
 let toastTimer;
 // the toast function
